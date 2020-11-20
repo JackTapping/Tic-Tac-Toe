a68bedafddae810280638cf04a5410aaa4b2f25f
Jack Tapping <52420708+JackTapping@users.noreply.github.com>
2020-11-20 17:30:17 +0000

    Update Main.cpp
    finished the Menu system for the game

--- Tic-Tac-Toe/Main.cpp
+++ Tic-Tac-Toe/Main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <Windows.h>
+#include <conio.h>
 
-// changing the colour of the text based on an int paramneter 
+// ========= functions to contorl the console window ========
 void ChangeTextColour(int iCoulorValue)
 {
     //0 = Black 
@@ -15,7 +16,7 @@ void ChangeTextColour(int iCoulorValue)
 
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), iCoulorValue);
 }
-void SetCursorPosition(int x, int y)
+void SetCursorPosition(short x, short y)
 {
     HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
     COORD pos = { x,y };
@@ -32,8 +33,8 @@ void ChangeConsoleWindowSize(int iXValue, int iYvalue)
 
 }
 
-
-void printBoarders()
+// ========== Functions to print out the load screen. ========= 
+void PrintBoarderAnimation()
 {
     ChangeTextColour(15);
 
@@ -137,7 +138,7 @@ void printBoarders()
         if (iLowerTopindex == 57) bBoardersDrawn = true;
     }
 }
-void PrintLoading()
+void PrintLoadingAnimation()
 {
 
     
@@ -145,12 +146,19 @@ void PrintLoading()
 
     ChangeTextColour(12);
     std::cout << "\t _                     _\n";
+    Sleep(75);
     std::cout << "\t| |                   | (_)             \n";
+    Sleep(75);
     std::cout << "\t| |     ___   __ _  __| |_ _ __   __ _  \n";
+    Sleep(75);
     std::cout << "\t| |    / _ \\ / _` |/ _` | | '_ \\ / _` | \n";
+    Sleep(75);
     std::cout << "\t| |___| (_) | (_| | (_| | | | | | (_| | \n";
+    Sleep(75);
     std::cout << "\t\\_____/\\___/ \\__,_|\\__,_|_|_| |_|\\__, | \n";
+    Sleep(75);
     std::cout << "\t                                  __/ | \n";
+    Sleep(75);
     std::cout << "\t                                 |___/  \n";
 
     Sleep(250);
@@ -190,11 +198,424 @@ void PrintLoading()
     Sleep(50);
     system("Pause");
 }
+void PrintLoadScreen()
+{
+    ChangeConsoleWindowSize(450, 350);
+    PrintBoarderAnimation();
+    PrintLoadingAnimation();
+    system("cls");
+}
+
+// ========== Functions to print out the Main Menu =========
+
+//Printing the menu 
+void printMenuTitleAnimation()
+{
+    int iSleep = 50;
+    Sleep(iSleep);
+    std::cout << "\t\t\t _    _\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t|  \\/  |                 \n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t| .  . | ___ _ __  _   _                 \n";
+    Sleep(iSleep);
+    std::cout << "\t          ===== | |\\/| |/ _ \\ '_ \\| | | | =====\n";
+    Sleep(iSleep);
+    std::cout << "\t     ========== | |  | |  __/ | | | |_| | ==========  \n";
+    Sleep(iSleep);
+    std::cout << "\t=============== \\_|  |_/\\___|_| |_|\\__,_| ===============\n";
+    Sleep(iSleep);
+}
+void PrintMenuOptionsAnimation()
+{
+    
+    int iSleep = 50;
+
+    std::cout << "\t\t\t __   __   ______ _   _______\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t/  |  \\ \\  | ___ \\ | | | ___ \\\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t`| |   | | | |_/ / | | | |_/ /\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t | |   | | |  __/| | | |  __/\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t_| |_  | | | |   \\ \\_/ / |\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t\\___/  | | \\_|    \\___/\\_|\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t      /_/             \n";
+    Sleep(iSleep);
+    std::cout << "\n";
+    std::cout << "\t\t\t _____  __    _____ ______ _   _ \n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t/ __  \\ \\ \\  /  __ \\| ___ \\ | | |\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t`' / /'  | | | /  \\/| |_/ / | | |\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t  / /    | | | |    |  __/| | | |\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t./ /___  | | | \\__/\\| |   | |_| |\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t\\_____/  | |  \\____/\\_|    \\___/\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t        /_/\n";
+    Sleep(iSleep);
+    std::cout << "\n";
+    std::cout << "\t\t\t _____  __    _____     _ _\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t|  ___| \\ \\  |  ___|   (_) |\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t| |__    | | | |____  ___| |_\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t|  __|   | | |  __\\ \\/ / | __|\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t| |___   | | | |___>  <| | |_\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t\\____/   | | \\____/_/\\_\\_|\\__|\n";
+    Sleep(iSleep);
+    std::cout << "\t\t\t        /_/\n";
+
+}
+void PrintMenu()
+{
+    ChangeConsoleWindowSize(700, 600);
+    ChangeTextColour(15);
+
+    printMenuTitleAnimation();
+    PrintMenuOptionsAnimation();
+}
+
+//flashing the option that the user inputs 
+void FlashPVPOption()
+{
+    ChangeTextColour(10);
+    SetCursorPosition(5, 6);
+    std::cout << "\t\t\t __   __   ______ _   _______";
+    SetCursorPosition(5, 7);
+    std::cout << "\t\t\t/  |  \\ \\  | ___ \\ | | | ___ \\";
+    SetCursorPosition(5, 8);
+    std::cout << "\t\t\t`| |   | | | |_/ / | | | |_/ /";
+    SetCursorPosition(5, 9);
+    std::cout << "\t\t\t | |   | | |  __/| | | |  __/";
+    SetCursorPosition(5, 10);
+    std::cout << "\t\t\t_| |_  | | | |   \\ \\_/ / |";
+    SetCursorPosition(5, 11);
+    std::cout << "\t\t\t\\___/  | | \\_|    \\___/\\_|";
+    SetCursorPosition(5, 12);
+    std::cout << "\t\t\t      /_/             ";
+
+    Sleep(350);
+
+    ChangeTextColour(15);
+    SetCursorPosition(5, 6);
+    std::cout << "\t\t\t __   __   ______ _   _______";
+    SetCursorPosition(5, 7);
+    std::cout << "\t\t\t/  |  \\ \\  | ___ \\ | | | ___ \\";
+    SetCursorPosition(5, 8);
+    std::cout << "\t\t\t`| |   | | | |_/ / | | | |_/ /";
+    SetCursorPosition(5, 9);
+    std::cout << "\t\t\t | |   | | |  __/| | | |  __/";
+    SetCursorPosition(5, 10);
+    std::cout << "\t\t\t_| |_  | | | |   \\ \\_/ / |";
+    SetCursorPosition(5, 11);
+    std::cout << "\t\t\t\\___/  | | \\_|    \\___/\\_|";
+    SetCursorPosition(5, 12);
+    std::cout << "\t\t\t      /_/             ";
+
+    Sleep(350);
+
+    ChangeTextColour(10);
+    SetCursorPosition(5, 6);
+    std::cout << "\t\t\t __   __   ______ _   _______";
+    SetCursorPosition(5, 7);
+    std::cout << "\t\t\t/  |  \\ \\  | ___ \\ | | | ___ \\";
+    SetCursorPosition(5, 8);
+    std::cout << "\t\t\t`| |   | | | |_/ / | | | |_/ /";
+    SetCursorPosition(5, 9);
+    std::cout << "\t\t\t | |   | | |  __/| | | |  __/";
+    SetCursorPosition(5, 10);
+    std::cout << "\t\t\t_| |_  | | | |   \\ \\_/ / |";
+    SetCursorPosition(5, 11);
+    std::cout << "\t\t\t\\___/  | | \\_|    \\___/\\_|";
+    SetCursorPosition(5, 12);
+    std::cout << "\t\t\t      /_/             ";
+
+    Sleep(350);
+
+    ChangeTextColour(15);
+    SetCursorPosition(5, 6);
+    std::cout << "\t\t\t __   __   ______ _   _______";
+    SetCursorPosition(5, 7);
+    std::cout << "\t\t\t/  |  \\ \\  | ___ \\ | | | ___ \\";
+    SetCursorPosition(5, 8);
+    std::cout << "\t\t\t`| |   | | | |_/ / | | | |_/ /";
+    SetCursorPosition(5, 9);
+    std::cout << "\t\t\t | |   | | |  __/| | | |  __/";
+    SetCursorPosition(5, 10);
+    std::cout << "\t\t\t_| |_  | | | |   \\ \\_/ / |";
+    SetCursorPosition(5, 11);
+    std::cout << "\t\t\t\\___/  | | \\_|    \\___/\\_|";
+    SetCursorPosition(5, 12);
+    std::cout << "\t\t\t      /_/             ";
+
+    Sleep(350);
+
+    ChangeTextColour(10);
+    SetCursorPosition(5, 6);
+    std::cout << "\t\t\t __   __   ______ _   _______";
+    SetCursorPosition(5, 7);
+    std::cout << "\t\t\t/  |  \\ \\  | ___ \\ | | | ___ \\";
+    SetCursorPosition(5, 8);
+    std::cout << "\t\t\t`| |   | | | |_/ / | | | |_/ /";
+    SetCursorPosition(5, 9);
+    std::cout << "\t\t\t | |   | | |  __/| | | |  __/";
+    SetCursorPosition(5, 10);
+    std::cout << "\t\t\t_| |_  | | | |   \\ \\_/ / |";
+    SetCursorPosition(5, 11);
+    std::cout << "\t\t\t\\___/  | | \\_|    \\___/\\_|";
+    SetCursorPosition(5, 12);
+    std::cout << "\t\t\t      /_/             ";
+}
+void FlashCPUOption()
+{
+    ChangeTextColour(10);
+    SetCursorPosition(5, 14);
+    std::cout << "\t\t\t _____  __    _____ ______ _   _ \n";
+    SetCursorPosition(5, 15);
+    std::cout << "\t\t\t/ __  \\ \\ \\  /  __ \\| ___ \\ | | |\n";
+    SetCursorPosition(5, 16);
+    std::cout << "\t\t\t`' / /'  | | | /  \\/| |_/ / | | |\n";
+    SetCursorPosition(5, 17);
+    std::cout << "\t\t\t  / /    | | | |    |  __/| | | |\n";
+    SetCursorPosition(5, 18);
+    std::cout << "\t\t\t./ /___  | | | \\__/\\| |   | |_| |\n";
+    SetCursorPosition(5, 19);
+    std::cout << "\t\t\t\\_____/  | |  \\____/\\_|    \\___/\n";
+    SetCursorPosition(5, 20);
+    std::cout << "\t\t\t        /_/\n";
+
+    Sleep(350);
+
+    ChangeTextColour(15);
+    SetCursorPosition(5, 14);
+    std::cout << "\t\t\t _____  __    _____ ______ _   _ \n";
+    SetCursorPosition(5, 15);
+    std::cout << "\t\t\t/ __  \\ \\ \\  /  __ \\| ___ \\ | | |\n";
+    SetCursorPosition(5, 16);
+    std::cout << "\t\t\t`' / /'  | | | /  \\/| |_/ / | | |\n";
+    SetCursorPosition(5, 17);
+    std::cout << "\t\t\t  / /    | | | |    |  __/| | | |\n";
+    SetCursorPosition(5, 18);
+    std::cout << "\t\t\t./ /___  | | | \\__/\\| |   | |_| |\n";
+    SetCursorPosition(5, 19);
+    std::cout << "\t\t\t\\_____/  | |  \\____/\\_|    \\___/\n";
+    SetCursorPosition(5, 20);
+    std::cout << "\t\t\t        /_/\n";
+
+    Sleep(350);
+
+    ChangeTextColour(10);
+    SetCursorPosition(5, 14);
+    std::cout << "\t\t\t _____  __    _____ ______ _   _ \n";
+    SetCursorPosition(5, 15);
+    std::cout << "\t\t\t/ __  \\ \\ \\  /  __ \\| ___ \\ | | |\n";
+    SetCursorPosition(5, 16);
+    std::cout << "\t\t\t`' / /'  | | | /  \\/| |_/ / | | |\n";
+    SetCursorPosition(5, 17);
+    std::cout << "\t\t\t  / /    | | | |    |  __/| | | |\n";
+    SetCursorPosition(5, 18);
+    std::cout << "\t\t\t./ /___  | | | \\__/\\| |   | |_| |\n";
+    SetCursorPosition(5, 19);
+    std::cout << "\t\t\t\\_____/  | |  \\____/\\_|    \\___/\n";
+    SetCursorPosition(5, 20);
+    std::cout << "\t\t\t        /_/\n";
+
+    Sleep(350);
+
+    ChangeTextColour(15);
+    SetCursorPosition(5, 14);
+    std::cout << "\t\t\t _____  __    _____ ______ _   _ \n";
+    SetCursorPosition(5, 15);
+    std::cout << "\t\t\t/ __  \\ \\ \\  /  __ \\| ___ \\ | | |\n";
+    SetCursorPosition(5, 16);
+    std::cout << "\t\t\t`' / /'  | | | /  \\/| |_/ / | | |\n";
+    SetCursorPosition(5, 17);
+    std::cout << "\t\t\t  / /    | | | |    |  __/| | | |\n";
+    SetCursorPosition(5, 18);
+    std::cout << "\t\t\t./ /___  | | | \\__/\\| |   | |_| |\n";
+    SetCursorPosition(5, 19);
+    std::cout << "\t\t\t\\_____/  | |  \\____/\\_|    \\___/\n";
+    SetCursorPosition(5, 20);
+    std::cout << "\t\t\t        /_/\n";
+
+    Sleep(350);
+
+    ChangeTextColour(10);
+    SetCursorPosition(5, 14);
+    std::cout << "\t\t\t _____  __    _____ ______ _   _ \n";
+    SetCursorPosition(5, 15);
+    std::cout << "\t\t\t/ __  \\ \\ \\  /  __ \\| ___ \\ | | |\n";
+    SetCursorPosition(5, 16);
+    std::cout << "\t\t\t`' / /'  | | | /  \\/| |_/ / | | |\n";
+    SetCursorPosition(5, 17);
+    std::cout << "\t\t\t  / /    | | | |    |  __/| | | |\n";
+    SetCursorPosition(5, 18);
+    std::cout << "\t\t\t./ /___  | | | \\__/\\| |   | |_| |\n";
+    SetCursorPosition(5, 19);
+    std::cout << "\t\t\t\\_____/  | |  \\____/\\_|    \\___/\n";
+    SetCursorPosition(5, 20);
+    std::cout << "\t\t\t        /_/\n";
+
+    Sleep(350);
+}
+void FlashExitOption() 
+{
+    ChangeTextColour(10);
+    SetCursorPosition(5, 22);
+    std::cout << "\t\t\t _____  __    _____     _ _\n";
+    SetCursorPosition(5, 23);
+    std::cout << "\t\t\t|  ___| \\ \\  |  ___|   (_) |\n";
+    SetCursorPosition(5, 24);
+    std::cout << "\t\t\t| |__    | | | |____  ___| |_\n";
+    SetCursorPosition(5, 25);
+    std::cout << "\t\t\t|  __|   | | |  __\\ \\/ / | __|\n";
+    SetCursorPosition(5, 26);
+    std::cout << "\t\t\t| |___   | | | |___>  <| | |_\n";
+    SetCursorPosition(5, 27);
+    std::cout << "\t\t\t\\____/   | | \\____/_/\\_\\_|\\__|\n";
+    SetCursorPosition(5, 28);
+    std::cout << "\t\t\t        /_/\n";
+
+    Sleep(350);
+
+    ChangeTextColour(15);
+    SetCursorPosition(5, 22);
+    std::cout << "\t\t\t _____  __    _____     _ _\n";
+    SetCursorPosition(5, 23);
+    std::cout << "\t\t\t|  ___| \\ \\  |  ___|   (_) |\n";
+    SetCursorPosition(5, 24);
+    std::cout << "\t\t\t| |__    | | | |____  ___| |_\n";
+    SetCursorPosition(5, 25);
+    std::cout << "\t\t\t|  __|   | | |  __\\ \\/ / | __|\n";
+    SetCursorPosition(5, 26);
+    std::cout << "\t\t\t| |___   | | | |___>  <| | |_\n";
+    SetCursorPosition(5, 27);
+    std::cout << "\t\t\t\\____/   | | \\____/_/\\_\\_|\\__|\n";
+    SetCursorPosition(5, 28);
+    std::cout << "\t\t\t        /_/\n";
+
+    Sleep(350);
+
+    ChangeTextColour(10);
+    SetCursorPosition(5, 22);
+    std::cout << "\t\t\t _____  __    _____     _ _\n";
+    SetCursorPosition(5, 23);
+    std::cout << "\t\t\t|  ___| \\ \\  |  ___|   (_) |\n";
+    SetCursorPosition(5, 24);
+    std::cout << "\t\t\t| |__    | | | |____  ___| |_\n";
+    SetCursorPosition(5, 25);
+    std::cout << "\t\t\t|  __|   | | |  __\\ \\/ / | __|\n";
+    SetCursorPosition(5, 26);
+    std::cout << "\t\t\t| |___   | | | |___>  <| | |_\n";
+    SetCursorPosition(5, 27);
+    std::cout << "\t\t\t\\____/   | | \\____/_/\\_\\_|\\__|\n";
+    SetCursorPosition(5, 28);
+    std::cout << "\t\t\t        /_/\n";
+
+    Sleep(350);
+
+    ChangeTextColour(15);
+    SetCursorPosition(5, 22);
+    std::cout << "\t\t\t _____  __    _____     _ _\n";
+    SetCursorPosition(5, 23);
+    std::cout << "\t\t\t|  ___| \\ \\  |  ___|   (_) |\n";
+    SetCursorPosition(5, 24);
+    std::cout << "\t\t\t| |__    | | | |____  ___| |_\n";
+    SetCursorPosition(5, 25);
+    std::cout << "\t\t\t|  __|   | | |  __\\ \\/ / | __|\n";
+    SetCursorPosition(5, 26);
+    std::cout << "\t\t\t| |___   | | | |___>  <| | |_\n";
+    SetCursorPosition(5, 27);
+    std::cout << "\t\t\t\\____/   | | \\____/_/\\_\\_|\\__|\n";
+    SetCursorPosition(5, 28);
+    std::cout << "\t\t\t        /_/\n";
+
+    Sleep(350);
+
+    ChangeTextColour(10);
+    SetCursorPosition(5, 22);
+    std::cout << "\t\t\t _____  __    _____     _ _\n";
+    SetCursorPosition(5, 23);
+    std::cout << "\t\t\t|  ___| \\ \\  |  ___|   (_) |\n";
+    SetCursorPosition(5, 24);
+    std::cout << "\t\t\t| |__    | | | |____  ___| |_\n";
+    SetCursorPosition(5, 25);
+    std::cout << "\t\t\t|  __|   | | |  __\\ \\/ / | __|\n";
+    SetCursorPosition(5, 26);
+    std::cout << "\t\t\t| |___   | | | |___>  <| | |_\n";
+    SetCursorPosition(5, 27);
+    std::cout << "\t\t\t\\____/   | | \\____/_/\\_\\_|\\__|\n";
+    SetCursorPosition(5, 28);
+    std::cout << "\t\t\t        /_/\n";
+
+    Sleep(350);
+}
+void FlashSelectedOption(char cInput)
+{
+    switch (cInput)
+    {
+    case '1':
+        FlashPVPOption();
+        break;
+    case '2':
+        FlashCPUOption();
+        break;
+    case 'e':
+    case 'E':
+        FlashExitOption();
+        break;
+    default:
+        Beep(500, 500);
+        break;
+    }
+
+}
+
+
+
 
 int main()
 {
-    ChangeConsoleWindowSize(450, 350);
-    printBoarders();
-    PrintLoading();
+    char cInput = ' ';
+
+    PrintLoadScreen();
+    PrintMenu();
+    std::cout << "\n\n";
+
+    cInput = _getch();
+    FlashSelectedOption(cInput);
+
+    switch (cInput)
+    {
+    case '1':
+        system("cls");
+        std::cout << "Menu option 1";
+        system("pause");
+        break;
+    case '2':
+        system("cls");
+        std::cout << "Menu option 2";
+        system("pause");
+        break;
+    case 'e':
+    case 'E':
+        system("cls");
+        system("pause");
+        break;
+    default:
+        Beep(500, 500);
+        break;
+    }
+
 	return 0;
 }
