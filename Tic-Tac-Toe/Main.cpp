3420e8298425d5ea166e5b3e83a7d494aee4e0d8
Jack Tapping <52420708+JackTapping@users.noreply.github.com>
2020-11-22 18:08:07 +0000

    Update Main.cpp
    Can now place pieces on the board and the game will go back to the main  menu if the board gets full

--- Tic-Tac-Toe/Main.cpp
+++ Tic-Tac-Toe/Main.cpp
@@ -2,7 +2,7 @@
 #include <Windows.h>
 #include <conio.h>
 
-// ========= functions to contorl the console window ========
+// ========= functions to control the console window ========
 void ChangeTextColour(int iCoulorValue)
 {
     //0 = Black 
@@ -206,7 +206,7 @@ void PrintLoadScreen()
     system("cls");
 }
 
-// ========== Functions to print out the Main Menu =========
+// ========== Functions to print out the Main Menu and get input for it =========
 
 //Printing the menu 
 void printMenuTitleAnimation()
@@ -283,6 +283,22 @@ void PrintMenu()
 
     printMenuTitleAnimation();
     PrintMenuOptionsAnimation();
+}
+char GetMenuInput()
+{
+    char cInput;
+    do
+    {
+        // will make a beeping noise every time the user fails to input a correct value 
+        // ony 1 2 or e will be accepted 
+        cInput = _getch();
+        if (cInput != '1' && cInput != '2' && cInput != 'e' && cInput != 'E')
+        {
+            Beep(500, 500);
+        }
+    } while (cInput != '1' && cInput != '2' && cInput != 'e' && cInput != 'E');
+
+    return cInput;
 }
 
 //flashing the option that the user inputs 
@@ -583,7 +599,7 @@ void FlashSelectedOption(char cInput)
 
 // ========= Functions for Printing Out the Game Screen =========
 
-//Printing out the Title Menu 
+
 void PrintGameTitle()
 {
     int iSleep = 100;
@@ -693,40 +709,415 @@ void PrintGameBoard()
     std::cout << "         | |          | |            \n"; Sleep(iSleep);
     std::cout << "         |_|          |_|         ";      Sleep(iSleep);
 }
+void PrintRules()
+{
+    int i = 10;
+    ChangeTextColour(13);
+    SetCursorPosition(55, i++);
+    std::cout << "   ___Rules___";
+
+    ChangeTextColour(15);
+    SetCursorPosition(55, i++);
+    std::cout << "You must get three ";
+    SetCursorPosition(55, i++);
+    std::cout << "in a row to win";
+    SetCursorPosition(55, i++);
+    std::cout << "";
+    SetCursorPosition(55, i++);
+    std::cout << "To make a move type a number";
+    SetCursorPosition(55, i++);
+    std::cout << "between 1 and 9 and your ";
+    SetCursorPosition(55, i++);
+    std::cout << "pice will be placed on the ";
+    SetCursorPosition(55, i++);
+    std::cout << "suqare that contains that number";
+    SetCursorPosition(55, i++);
+
+    std::cout << " ";
+    SetCursorPosition(55, i++);
+    std::cout << "Player 1 = ";
+    ChangeTextColour(11);
+    std::cout << "X";
+    ChangeTextColour(15);
+    SetCursorPosition(55, i++);
+
+    std::cout << "Player 2 = ";
+    ChangeTextColour(12);
+    std::cout << "O";
+
+    SetCursorPosition(55, i++);
+    SetCursorPosition(55, i++);
+    ChangeTextColour(13);
+    std::cout << "    ___Win___";
+
+    ChangeTextColour(15);
+    SetCursorPosition(55, i++);
+    std::cout << "XXX   ...   ...";
+    SetCursorPosition(55, i++);
+    std::cout << "...   XXX   ...";
+    SetCursorPosition(55, i++);
+    std::cout << "...   ...   XXX";
+    SetCursorPosition(55, i++);
+    SetCursorPosition(55, i++);
+    std::cout << "X..   .X.   ..X";
+    SetCursorPosition(55, i++);
+    std::cout << "X..   .X.   ..X";
+    SetCursorPosition(55, i++);
+    std::cout << "X..   .X.   ..X";
+    SetCursorPosition(55, i++);
+    SetCursorPosition(55, i++);
+    std::cout << "   ..X   X..";
+    SetCursorPosition(55, i++);
+    std::cout << "   .X.   .X.";
+    SetCursorPosition(55, i++);
+    std::cout << "   X..   ..X";
+    SetCursorPosition(55, i++);
+
+    
+}
+void InitializeGame()
+{
+    system("cls");
+    PrintGameTitle();
+    PrintGameBoard();
+    PrintRules();
+}
+
+
+void PrintXPice(int iXOffSet, int iYOffSet) 
+{
+    ChangeTextColour(11);
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << "__   __ ";
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << "\\ \\ / /";
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << " \\ V /  ";
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << " /   \\  ";
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << "/ /^\\ \\";
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << "\\/   \\/ ";
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << "";
+}
+void PrintYPice(int iXOffSet, int iYOffSet)
+{
+    ChangeTextColour(12);
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << " _____ ";
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << "|  _  |";
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << "| | | |";
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << "| | | |";
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << "\\ \\_/ /";
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << " \\___/ ";
+    SetCursorPosition(iXOffSet, iYOffSet++);
+    std::cout << "";
+}
+void UpdateGameVisuals(char cInput, int iPlayerturn)
+{
+    int iXOffset = 0;
+    int iYOffset = 0;
+
+    if(cInput == '1')
+    {
+        iXOffset = 1;
+        iYOffset = 7;
+
+        if (iPlayerturn == 0)
+        {
+            PrintXPice(iXOffset, iYOffset);
+        }
+        else
+        {
+            PrintYPice(iXOffset, iYOffset);
+        }
+        
+    }
+    if (cInput == '2')
+    {
+        iXOffset = 14;
+        iYOffset = 7;
+
+        if (iPlayerturn == 0)
+        {
+            PrintXPice(iXOffset, iYOffset);
+        }
+        else
+        {
+            PrintYPice(iXOffset, iYOffset);
+        }
+
+    }
+    if (cInput == '3')
+    {
+        iXOffset = 27;
+        iYOffset = 7;
+
+        if (iPlayerturn == 0)
+        {
+            PrintXPice(iXOffset, iYOffset);
+        }
+        else
+        {
+            PrintYPice(iXOffset, iYOffset);
+        }
+
+    }
+    if (cInput == '4')
+    {
+        iXOffset = 1;
+        iYOffset = 17;
+
+        if (iPlayerturn == 0)
+        {
+            PrintXPice(iXOffset, iYOffset);
+        }
+        else
+        {
+            PrintYPice(iXOffset, iYOffset);
+        }
+
+    }
+    if (cInput == '5')
+    {
+        iXOffset = 14;
+        iYOffset = 17;
+
+        if (iPlayerturn == 0)
+        {
+            PrintXPice(iXOffset, iYOffset);
+        }
+        else
+        {
+            PrintYPice(iXOffset, iYOffset);
+        }
+
+    }
+    if (cInput == '6')
+    {
+        iXOffset = 27;
+        iYOffset = 17;
+
+        if (iPlayerturn == 0)
+        {
+            PrintXPice(iXOffset, iYOffset);
+        }
+        else
+        {
+            PrintYPice(iXOffset, iYOffset);
+        }
+
+    }
+    if (cInput == '7')
+    {
+        iXOffset = 1;
+        iYOffset = 27;
+
+        if (iPlayerturn == 0)
+        {
+            PrintXPice(iXOffset, iYOffset);
+        }
+        else
+        {
+            PrintYPice(iXOffset, iYOffset);
+        }
+
+    }
+    if (cInput == '8')
+    {
+        iXOffset = 14;
+        iYOffset = 27;
+
+        if (iPlayerturn == 0)
+        {
+            PrintXPice(iXOffset, iYOffset);
+        }
+        else
+        {
+            PrintYPice(iXOffset, iYOffset);
+        }
+
+    }
+    if (cInput == '9')
+    {
+        iXOffset = 27;
+        iYOffset = 27;
+
+        if (iPlayerturn == 0)
+        {
+            PrintXPice(iXOffset, iYOffset);
+        }
+        else
+        {
+            PrintYPice(iXOffset, iYOffset);
+        }
+
+    }
+    
+
+
+    
+}
+
+
+
+// ======== Functions for the Games Logic =========
+bool IsSapceFree(char cInput, char boardArray[3][3])
+{
+    if (cInput == '1')
+    {
+        return boardArray[0][0] == '.' ? true :  false;
+    }
+    else if (cInput == '2')
+    {
+        return boardArray[0][1] == '.' ? true : false;
+    }
+    else if (cInput == '3')
+    {
+        return boardArray[0][2] == '.' ? true : false;
+    }
+    else if (cInput == '4')
+    {
+        return boardArray[1][0] == '.' ? true : false;
+    }
+    else if (cInput == '5')
+    {
+        return boardArray[1][1] == '.' ? true : false;
+    }
+    else if (cInput == '6')
+    {
+        return boardArray[1][2] == '.' ? true : false;
+    }
+    else if (cInput == '7')
+    {
+        return boardArray[2][0] == '.' ? true : false;
+    }
+    else if (cInput == '8')
+    {
+        return boardArray[2][1] == '.' ? true : false;
+    }
+    else if (cInput == '9')
+    {
+        return boardArray[2][2] == '.' ? true : false;
+    }
+    
+}
+void UpdateGameState(int PlayerTurn, char cInput, char BoardArray[3][3])
+{
+    if (cInput == '1')
+    {
+        PlayerTurn == 0 ? BoardArray[0][0] = 'x': BoardArray[0][0] = 'o';
+    }
+    else if (cInput == '2')
+    {
+        PlayerTurn == 0 ? BoardArray[0][1] = 'x' : BoardArray[0][1] = 'o';
+    }
+    else if (cInput == '3')
+    {
+        PlayerTurn == 0 ? BoardArray[0][2] = 'x' : BoardArray[0][2] = 'o';
+    }
+    else if (cInput == '4')
+    {
+        PlayerTurn == 0 ? BoardArray[1][0] = 'x' : BoardArray[1][0] = 'o';
+    }
+    else if (cInput == '5')
+    {
+        PlayerTurn == 0 ? BoardArray[1][1] = 'x' : BoardArray[1][1] = 'o';
+    }
+    else if (cInput == '6')
+    {
+        PlayerTurn == 0 ? BoardArray[1][2] = 'x' : BoardArray[1][2] = 'o';
+    }
+    else if (cInput == '7')
+    {
+        PlayerTurn == 0 ? BoardArray[2][0] = 'x' : BoardArray[2][0] = 'o';
+    }
+    else if (cInput == '8')
+    {
+        PlayerTurn == 0 ? BoardArray[2][1] = 'x' : BoardArray[2][1] = 'o';
+    }
+    else if (cInput == '9')
+    {
+        PlayerTurn == 0 ? BoardArray[2][2] = 'x' : BoardArray[2][2] = 'o';
+    }
+}
+bool IsBoardFull(char BoardArray[3][3])
+{
+    for (int  i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3 ; j++)
+        {
+            if (BoardArray[i][j] == '.') 
+            {
+                return true;
+            }
+            
+        }
+    }
+    return false;
+}
 
 
 
 
 int main()
 {
-    char cInput = ' ';
+    //startting Variables 
+    char cInput = ' '; // is the only veriable used to get input 
+    int iPlayerTurn = 0; // 0 = player ones turn || 2 = playe twos turn 
+  
+    bool bKeepPlaying = true;
+    bool bFreeSpace = false;
 
+    char GameStateArray[3][3] = { '.','.','.',    '.','.','.',    '.','.','.', };
+    
+    // laoding the game and getting menu input fromt he user 
     PrintLoadScreen();
     PrintMenu();
-    std::cout << "\n\n";
+    cInput = GetMenuInput();
+    FlashSelectedOption(cInput);
 
-    do
-    {
-        cInput = _getch();
-        if (cInput != '1' && cInput != '2' && cInput != 'e' && cInput != 'E')
-        {
-            Beep(500, 500);
-        }
-    } while (cInput != '1' && cInput != '2' && cInput != 'e' && cInput != 'E');
-    
     while (cInput != 'e' &&  cInput != 'E')
     {
-        
-        FlashSelectedOption(cInput);
-    
-    
         switch (cInput)
         {
         case '1':
-            system("cls");
-            PrintGameTitle();
-            PrintGameBoard();
-            _getch();
+            InitializeGame();
+
+            while (bKeepPlaying == true)
+            {
+                if (bKeepPlaying == true)
+                {
+                    while (bFreeSpace == false)
+                    {
+                        cInput = _getch();
+                        bFreeSpace = IsSapceFree(cInput, GameStateArray);
+                    }
+                }
+
+                UpdateGameState(iPlayerTurn, cInput, GameStateArray);
+                UpdateGameVisuals(cInput, iPlayerTurn);
+
+                if (iPlayerTurn == 0)
+                {
+                    iPlayerTurn++;
+                }
+                else
+                {
+                    iPlayerTurn--;
+                }
+
+                bFreeSpace = false;
+                bKeepPlaying = IsBoardFull(GameStateArray);
+            }
+            
             system("cls");
             break;
         case '2':
@@ -738,16 +1129,9 @@ int main()
         }
 
         PrintMenu();
+        cInput = GetMenuInput();
+        FlashSelectedOption(cInput);
         std::cout << "\n\n";
-
-        do
-        {
-            cInput = _getch();
-            if (cInput != '1' && cInput != '2' && cInput != 'e' && cInput != 'E')
-            {
-                Beep(500, 500);
-            }
-        } while (cInput != '1' && cInput != '2' && cInput != 'e' && cInput != 'E');
     }
 
     system("cls");
